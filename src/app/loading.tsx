002be172bde397dfc24b0d51cdc2c28ea23e7922
sebastien <sebastien.pingal@gmail.com>
2024-03-12 13:17:42 +0100

    feat(Graphql): dynamic call

---
+++ src/app/loading.tsx
@@ -0,0 +1,10 @@
+import { LoaderCircle } from 'lucide-react'
+import React from 'react'
+
+const Loading = () => {
+  return (
+    <LoaderCircle className="w-8 h-8" />
+  )
+}
+
+export default Loading
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,34 +1,106 @@
-import { nhost } from "@/app/lib/nhost";
+'use client'
+import { nhost } from "@/app/lib/nhost"
+import { Label } from "@/components/ui/label"
+import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group"
+import { LoaderCircle } from "lucide-react"
+import { useEffect, useState } from "react"
 
-export default async function Home() {
-  // TODO handle type for historyEntries
-  const query = `
-    query GetHistoryentries {
-      historyentries(limit: 100) {
-        is_read
-        subject
-        type
-        created_at
-        user_id
-        linkedin_seat_id
+export default function Home() {
+  const [historyEntries, setHistoryEntries] = useState([])
+  const [selectedMonths, setSelectedMonths] = useState("3")
+  const [loading, setLoading] = useState(true)
+  const [error, setError] = useState(null)
+
+  useEffect(() => {
+    console.log('selectedMonths', selectedMonths)
+    setLoading(true)
+    setError(null)
+
+    const fetchHistoryEntries = async () => {
+      console.log('fetchHistoryEntries')
+      const now = new Date()
+      const startDate = new Date(now.getFullYear(), now.getMonth() - parseInt(selectedMonths), now.getDate()).toISOString()
+      let query = ``
+
+      if (selectedMonths === '999') {
+        query = `
+          query GetHistoryentries {
+          historyentries {
+            is_read
+            subject
+            type
+            created_at
+            user_id
+            linkedin_seat_id
+          }
+        }
+      `
+      } else {
+        query = `
+          query GetHistoryentries {
+          historyentries(
+            where: {
+              created_at: {
+                _gte: "${startDate}",
+              }
+            }) {
+            is_read
+            subject
+            type
+            created_at
+            user_id
+            linkedin_seat_id
+          }
+        }
+      `
       }
+
+      const { data, error } = await nhost.graphql.request(query)
+      if (error) {
+        console.error('error', error)
+        setError(error)
+      } else {
+        console.log(data)
+        setHistoryEntries(data?.historyentries || [])
+      }
+      setLoading(false)
     }
-  `
 
-  const {data, error} = await nhost.graphql.request(query)
-  if (error) {
-    console.error('error', error)
-  } else {
-    console.log(data)
-  }
-
+    fetchHistoryEntries()
+  }, [selectedMonths])
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="text-4xl font-bold">Nhost Auth History</h1>
-      <ul>
-        {JSON.stringify(data)}
-      </ul>
+    <main className="flex min-h-screen flex-col items-center gap-8 p-8">
+      <h1 className="text-4xl font-bold">Statistique</h1>
+      <RadioGroup defaultValue={selectedMonths} onValueChange={setSelectedMonths}>
+        <div className="flex items-center space-x-2">
+          <RadioGroupItem value="1" id="r1" />
+          <Label htmlFor="r1">Le dernier mois</Label>
+        </div>
+        <div className="flex items-center space-x-2">
+          <RadioGroupItem value="3" id="r2" />
+          <Label htmlFor="r2">Les 3 derniers mois</Label>
+        </div>
+        <div className="flex items-center space-x-2">
+          <RadioGroupItem value="12" id="r3" />
+          <Label htmlFor="r3">la dernière année</Label>
+        </div>
+        <div className="flex items-center space-x-2">
+          <RadioGroupItem value="999" id="r4" />
+          <Label htmlFor="r4">Tout</Label>
+        </div>
+      </RadioGroup>
+      {loading ? (
+        <LoaderCircle className="w-8 h-8 animate-spin" />
+      ) : error ? (
+        <div>{JSON.stringify(error)}</div>
+      ) : historyEntries.length > 0 ? (
+        <div>
+          {historyEntries.length}
+        </div>
+      ) : (
+        <div>Aucune donnée...</div>
+      )}
     </main>
-  );
+  )
 }
